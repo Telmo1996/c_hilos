48f4cec41d071aed6b92c4302e393b0023c66277
Telmo1996 <tfercor@gmail.com>
2020-11-14 22:54:06 +0100

    image to json done

--- py/imageToJson.py
+++ py/imageToJson.py
@@ -1,7 +1,16 @@
 import json
+from json import JSONEncoder
 import sys
 from PIL import Image
+import numpy
 from numpy import asarray
+
+
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, numpy.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self, obj)
 
 
 if __name__ == '__main__':
@@ -17,6 +26,13 @@ if __name__ == '__main__':
 	img = asarray(image)
 	data = img.copy()
 
-	for i in range(0, height):
-		for j in range(0, width):
-			print(data[i][j])
+	#for i in range(0, height):
+	#	for j in range(0, width):
+	#		print(data[i][j])
+
+	miJson = {"info": {"width": width, "height": height},
+			"data": data}
+	encodedMiJson = json.dumps(miJson, cls=NumpyArrayEncoder)
+	with open("out.json", 'w') as outfile:
+		json.dump(encodedMiJson, outfile)
+
